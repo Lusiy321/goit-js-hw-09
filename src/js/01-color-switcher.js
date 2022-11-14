9d04713c192b62c01aeae3692c5c8ab8c1be7668
Volodymyr Lysenko <lusiy321@gmail.com>
2022-11-14 12:53:42 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('[data-page]');
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopColor);
+
+let attribute = null;
+
+function changeColor() {
+  attribute = setInterval(() => {
+    const random = getRandomHexColor();
+    body.style.backgroundColor = `${random}`;
+  }, 1000);
+  startBtn.disabled = !startBtn.disabled;
+}
+
+function stopColor() {
+  if (startBtn.disabled) {
+    startBtn.disabled = !startBtn.disabled;
+    clearInterval(attribute);
+  }
+}
